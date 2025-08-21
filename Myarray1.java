e056880c5ff3ab7243d8525f7c4209378e0efd1a
manishbeth18 <manish.beth@gmail.com>
2025-08-21 18:21:41 +0530

    Add files via upload

---
+++ Myarray1.java
@@ -0,0 +1,16 @@
+public class Myarray1 {
+    public static void main(String[] args){
+        int[] n = new int[5];
+        n[0]=1;
+        n[1]=2;
+        n[2]=3;
+        n[3]=4;
+        n[4]=5;
+        System.out.println(n[4]);
+
+    }
+
+}
+
+
+
